8f063c5f372a14f656acc72e8d0b2ef8f80c138a
jflash1973 <davide.pelosi@gmail.com>
2015-05-29 13:01:52 +0200

    Added CKAN Constants

---
+++ src/main/java/com/sciamlab/ckan4j/util/CKAN.java
@@ -0,0 +1,28 @@
+package com.sciamlab.ckan4j.util;
+
+public class CKAN {
+	public static final String ID = "id";
+	public static final String RESOURCES = "resources";
+	public static final String TAGS = "tags";
+	public static final String METADATA_MODIFIED = "metadata_modified";
+	public static final String NAME = "name";
+	public static final String TITLE = "title";
+	public static final String URL = "url";
+	public static final String AUTHOR = "author";
+	public static final String AUTHOR_EMAIL = "author_email";
+	public static final String MAINTAINER = "maintainer";
+	public static final String MAINTAINER_EMAIL = "maintainer_email";
+	public static final String NOTES = "notes";
+	public static final String METADATA_CREATED = "metadata_created";
+	public static final String LICENSE_ID = "license_id";
+	public static final String LICENSE_URL = "license_url";
+	public static final String LICENSE_NAME = "license_name";
+	public static final String LICENSE_TITLE = "license_title";
+	public static final String FORMAT = "format";
+	public static final String DESCRIPTION = "description";
+	public static final String EXTRAS = "extras";
+	public static final String SPATIAL = "spatial";
+	public static final String OWNER_ORG = "owner_org";
+	public static final String STATE = "state";
+	public static final String IMAGE_URL = "image_url";
+}
