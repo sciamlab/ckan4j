490cdf456f4bb82c799c1c36957afb2f287c1991
pstarace <paolostarace@gmail.com>
2015-06-26 16:04:11 +0200

    bug fix

--- src/main/java/com/sciamlab/ckan4j/CKANLogin.java
+++ src/main/java/com/sciamlab/ckan4j/CKANLogin.java
@@ -29,16 +29,10 @@ import com.sciamlab.common.util.SciamlabHashUtils;
 
 public class CKANLogin {
 	
-	private final long time;
-	private final boolean remember;
-	private final String user;
 	private final String secret;
 	private final String ckan_endpoint;
 
 	private CKANLogin(CKANLoginBuilder builder) {
-		this.time = builder.time;
-		this.remember = builder.remember;
-		this.user = builder.user;
 		this.secret = builder.secret;
 		this.ckan_endpoint = builder.ckan_endpoint;
 	}
@@ -47,15 +41,23 @@ public class CKANLogin {
 	 * 
 	 * @return the login form
 	 */
-	public Response login() {
-		String password_hashed = SciamlabHashUtils.signStringSHA1(time/1000+user+secret);
+	public Response login(String user) {
+		return this.login(user, false);
+	}
+	/**
+	 * 
+	 * @return the login form
+	 */
+	public Response login(String user, boolean remember) {
+		Date now = new Date();
+		String password_hashed = SciamlabHashUtils.signStringSHA1(now.getTime()/1000+user+secret);
 		StringBuffer form = new StringBuffer();
 		form.append("<html>");
 		form.append("<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /></head>");
 		form.append("<body onload=\"document.BizPassRedirectForm.submit()\" >");
 		form.append("<form name=\"BizPassRedirectForm\" action=\""+ckan_endpoint+"/login_generic?came_from=/user/logged_in\" method=\"post\">");
 		form.append("<input type=\"hidden\" id=\"field-login\" type=\"text\" name=\"login\" value=\""+user+"\" placeholder=\"\"  />");
-		form.append("<input type=\"hidden\" id=\"field-password\" type=\"password\" name=\"password\" value=\""+time/1000+password_hashed+"\" placeholder=\"\"  />");
+		form.append("<input type=\"hidden\" id=\"field-password\" type=\"password\" name=\"password\" value=\""+now.getTime()/1000+password_hashed+"\" placeholder=\"\"  />");
 		if(remember)
 			form.append("<input type=\"hidden\" id=\"field-remember\" type=\"checkbox\" name=\"remember\" value=\"63072000\" checked />");
 		form.append("</form>");
@@ -82,32 +84,17 @@ public class CKANLogin {
 	
 	public static class CKANLoginBuilder{
 		
-		private long time;
-		private boolean remember = false;
-		private final String user;
 		private final String secret;
 		private final String ckan_endpoint;
 		
-		public static CKANLoginBuilder getInstance(String user, String secret, String ckan_endpoint){
-			return new CKANLoginBuilder(user, secret, ckan_endpoint);
+		public static CKANLoginBuilder getInstance(String ckan_endpoint, String secret){
+			return new CKANLoginBuilder(ckan_endpoint, secret);
 		}
 		
-		private CKANLoginBuilder(String user, String secret, String ckan_endpoint) {
+		private CKANLoginBuilder(String ckan_endpoint, String secret) {
 			super();
-			this.user = user;
 			this.secret = secret;
 			this.ckan_endpoint = ckan_endpoint;
-			this.time = new Date().getTime();
-		}
-
-		public CKANLoginBuilder time(long time){
-			this.time = time;
-			return this;
-		}
-		
-		public CKANLoginBuilder remember(boolean remember){
-			this.remember = remember;
-			return this;
 		}
 
 		public CKANLogin build() {
