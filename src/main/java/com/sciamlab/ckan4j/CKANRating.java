1f480fd0c762c090832c76cd4157be54273561d1
pstarace <paolostarace@gmail.com>
2015-06-28 15:16:02 +0200

    bug fix

--- src/main/java/com/sciamlab/ckan4j/CKANRating.java
+++ src/main/java/com/sciamlab/ckan4j/CKANRating.java
@@ -30,6 +30,7 @@ import com.sciamlab.ckan4j.CKANApiClient.CKANApiClientBuilder;
 import com.sciamlab.ckan4j.exception.CKANException;
 import com.sciamlab.common.dao.SciamlabDAO;
 import com.sciamlab.common.exception.InternalServerErrorException;
+import com.sciamlab.common.util.SciamlabCollectionUtils;
 
 public class CKANRating {
 	
@@ -59,9 +60,17 @@ public class CKANRating {
 	public JSONObject getRate(String dataset_id) throws CKANException {
 		//getting dataset info
 		JSONObject dataset = this.ckan.packageShow(dataset_id);
-		
-		Integer rating_count = dataset.opt("rating_count")!=null && !"None".equals(dataset.getString("rating_count")) ? dataset.getInt("rating_count") : new Integer(0);
-		Integer rating_average_int = dataset.opt("rating_average_int")!=null && !"None".equals(dataset.getString("rating_average_int")) ? dataset.getInt("rating_average_int") : new Integer(0);
+		Integer rating_count = 0, rating_average_int = 0;
+		Double rating_average = 0.0;
+		for(Object tmp : SciamlabCollectionUtils.asList(dataset.getJSONArray("extras"))){
+			JSONObject extra = (JSONObject)tmp;
+			if("rating_count".equals(extra.getString("key")))
+					rating_count = extra.getInt("value");
+			else if("rating_average_int".equals(extra.getString("key")))
+				rating_average_int = extra.getInt("value");
+			else if("rating_average".equals(extra.getString("key")) && !"None".equals(extra.getInt("value")))
+					rating_average = extra.getDouble("value");
+		}
 		logger.debug("current count: "+rating_count+" average: "+rating_average_int);
 		
     	JSONObject json = new JSONObject();
@@ -85,37 +94,31 @@ public class CKANRating {
 		if(user_id==null || "".equals(user_id))
 			throw new Exception("User is mandatory");
 		
-//		//getting dataset info
-//		JSONObject dataset = this.ckan.packageShow(dataset_id);
-//		
-//		Integer rating_count = dataset.opt("rating_count")!=null && !"None".equals(dataset.getString("rating_count")) ? dataset.getInt("rating_count") : new Integer(0);
-//		Double rating_average = dataset.opt("rating_average")!=null && !"None".equals(dataset.getString("rating_average")) ? dataset.getDouble("rating_average") : new Double(0.0);
-//		logger.debug("current count: "+rating_count+" average: "+rating_average);
-		
-//		//converting spatial into string to avoid parsing issue during update
-//		if(dataset.opt("spatial")!=null && !"".equals(dataset.getString("spatial"))){
-//			JSONObject spatial = new JSONObject(dataset.getString("spatial"));
-//			if(spatial.opt("type")!=null && spatial.opt("coordinates")!=null);{
-//				dataset.put("spatial", "{ \"type\": \""+spatial.getString("type")+"\", \"coordinates\": "+ spatial.getJSONArray("coordinates") +" }");
-//			}
-//		}
-			
-		//getting user info
-//		JSONObject user = this.ckan.userShow(user_id);
+		//getting dataset info
+		JSONObject dataset = this.ckan.packageShow(dataset_id);
+		for(Object tmp : SciamlabCollectionUtils.asList(dataset.getJSONArray("extras"))){
+			JSONObject extra = (JSONObject)tmp;
+			if("spatial".equals(extra.getString("key"))){
+				//converting spatial into string to avoid parsing issue during update
+				JSONObject spatial = new JSONObject(extra.getString("value"));
+				if(spatial.opt("type")!=null && spatial.opt("coordinates")!=null)
+					extra.put("value", "{ \"type\": \""+spatial.getString("type")+"\", \"coordinates\": "+ spatial.getJSONArray("coordinates") +" }");
+				break;
+			}
+		}
 		
 		//once checked, then register the rating
-//		final String now_string = SciamlabDateUtils.getCurrentDateAsFormattedString("yyyy-MM-dd hh:mm:ss");
-		final Date now_string = new Date(new java.util.Date().getTime());
+		final Date now = new Date(new java.util.Date().getTime());
 		//update rating
 		int sql_result = dao.execUpdate("UPDATE " + this.rating_table
 				+ " SET rating = ?, modified = ?"
-				+ " WHERE user_id = ? AND package_id = ?;", new ArrayList<Object>(){{add(rating); add(now_string); add(user_id); add(dataset_id);}});
+				+ " WHERE user_id = ? AND package_id = ?;", new ArrayList<Object>(){{add(rating); add(now); add(user_id); add(dataset_id);}});
 		if(sql_result==0){
 			logger.debug("No existing rating found for user '"+user_id+"' on dataset '"+dataset_id+"'. Need to create a new one");
 			//insert new rating
 			sql_result = dao.execUpdate("INSERT INTO " + this.rating_table + " (user_id, package_id, rating, created, modified)"
 					+ " VALUES (?, ?, ?, ?, ?);", 
-					new ArrayList<Object>(){{add(user_id); add(dataset_id); add(rating); add(now_string); add(now_string);}});
+					new ArrayList<Object>(){{add(user_id); add(dataset_id); add(rating); add(now); add(now);}});
 		}
 		
 		//calculating the new average
@@ -127,29 +130,42 @@ public class CKANRating {
     	Double rating_average = ((BigDecimal) p.get("rating")).doubleValue();
     	Integer rating_count = ((Long) p.get("count")).intValue();
     	
-    	//getting dataset info
-		JSONObject dataset = this.ckan.packageShow(dataset_id);
-		//converting spatial into string to avoid parsing issue during update
-		if(dataset.opt("spatial")!=null && !"".equals(dataset.getString("spatial"))){
-			JSONObject spatial = new JSONObject(dataset.getString("spatial"));
-			if(spatial.opt("type")!=null && spatial.opt("coordinates")!=null);{
-				dataset.put("spatial", "{ \"type\": \""+spatial.getString("type")+"\", \"coordinates\": "+ spatial.getJSONArray("coordinates") +" }");
+		int rating_average_int = this.roundAverageToInteger(rating_average);
+    	boolean rating_count_found = false, rating_average_int_found = false, rating_average_found = false;
+    	for(Object tmp : SciamlabCollectionUtils.asList(dataset.getJSONArray("extras"))){
+			JSONObject extra = (JSONObject)tmp;
+			if("rating_count".equals(extra.getString("key"))){
+				extra.put("value", rating_count);
+				rating_count_found = true;
+			}else if("rating_average_int".equals(extra.getString("key"))){
+				extra.put("value", rating_average_int);
+				rating_average_int_found = true;
+			}else if("rating_average".equals(extra.getString("key"))){
+				extra.put("value", rating_average);
+				rating_average_found = true;
+			}else if("spatial".equals(extra.getString("key"))){
+				//converting spatial into string to avoid parsing issue during update
+				JSONObject spatial = new JSONObject(extra.getString("value"));
+				if(spatial.opt("type")!=null && spatial.opt("coordinates")!=null)
+					dataset.put("spatial", "{ \"type\": \""+spatial.getString("type")+"\", \"coordinates\": "+ spatial.getJSONArray("coordinates") +" }");
 			}
 		}
+    	if(!rating_count_found)
+    		dataset.getJSONArray("extras").put(new JSONObject().put("key", "rating_count").put("value", rating_count));
+    	if(!rating_average_int_found)
+    		dataset.getJSONArray("extras").put(new JSONObject().put("key", "rating_average_int").put("value", rating_average_int));
+    	if(!rating_average_found)
+    		dataset.getJSONArray("extras").put(new JSONObject().put("key", "rating_average").put("value", rating_average));
     	
-		//updating rating on CKANApiClient
-    	dataset.put("rating_average", rating_average);
-    	int average_rating_int = this.roundAverageToInteger(rating_average);
-    	dataset.put("rating_average_int", average_rating_int);
-    	dataset.put("rating_count", rating_count);
+    	//updating rating on CKANApiClient
     	dataset = this.ckan.packageUpdate(dataset);
     	logger.debug(dataset);
-		logger.info("Rating updated on CKANApiClient: avg "+dataset.getDouble("rating_average")+" count "+dataset.getInt("rating_count")+" ["+dataset_id+"]");
+		logger.info("Rating updated on CKANApiClient: avg "+rating_average_int+" count "+rating_count+" ["+dataset_id+"]");
     	
     	JSONObject json = new JSONObject();
     	json.put("dataset", dataset_id);
 		json.put("count", rating_count);
-		json.put("rating", average_rating_int);
+		json.put("rating", rating_average_int);
 		return json;
 	}
 
